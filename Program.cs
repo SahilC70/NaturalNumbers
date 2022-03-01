99fd70b4d9d61ba93fcbee08997be8c742fda073
Sahil Soni <sahil.soni@ibm.com>
2022-03-01 15:05:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, sum = 0;
+
+            Console.WriteLine("Display n terms of natural number and their sum:\n");
+            Console.Write("Input Value of terms : ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("\nThe first {0} natural number are :\n", n);
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", i);
+                sum += i;
+            }
+            Console.WriteLine("\nThe Sum of Natural Number upto {0} terms : {1}", n, sum);
+            Console.ReadLine();
+        }
+    }
+}
